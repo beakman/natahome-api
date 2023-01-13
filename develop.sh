50b4df8f8f92f4006c392c3c7d52bb0f3ee06955
Paco Salido <psalido@gmail.com>
2023-01-14 00:07:05 +0100

    Upgrade to 1.7.3

--- develop.sh
+++ develop.sh
@@ -3,5 +3,8 @@
 #Run migrations to ensure the database is updated
 medusa migrations run
 
+# Required for upgrade to 1.7.3
+node ./node_modules/@medusajs/medusa/dist/scripts/sales-channels-migration.js
+
 #Start development environment
 medusa develop
